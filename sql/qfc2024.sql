12dec2fd8b93ba4c954212c03117910a47f94dce
戈 张 <921519274@qq.com>
2024-06-29 19:49:25 +0800

    移动sql文件位置

---
+++ sql/qfc2024.sql
@@ -0,0 +1,74 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : test
+ Source Server Type    : MySQL
+ Source Server Version : 80029
+ Source Host           : localhost:3306
+ Source Schema         : qfc2024
+
+ Target Server Type    : MySQL
+ Target Server Version : 80029
+ File Encoding         : 65001
+
+ Date: 29/06/2024 19:47:51
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for t_file_info
+-- ----------------------------
+DROP TABLE IF EXISTS `t_file_info`;
+CREATE TABLE `t_file_info`  (
+  `id` int unsigned NOT NULL,
+  `filename` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '文件名',
+  `version` int unsigned NOT NULL COMMENT '版本',
+  `create_user` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '创建用户',
+  `update_user` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '更新用户',
+  `create_time` timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '更新时间',
+  `is_delete` tinyint(0) NOT NULL DEFAULT 0 COMMENT '是否删除',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `Index_filename`(`filename`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for t_file_line
+-- ----------------------------
+DROP TABLE IF EXISTS `t_file_line`;
+CREATE TABLE `t_file_line`  (
+  `uuid` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `file_id` int(0) NOT NULL COMMENT '文件id',
+  `version` int unsigned NOT NULL COMMENT '文件版本',
+  `line` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci COMMENT '行内容',
+  `decrypt_line` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci COMMENT '解密后的行内容',
+  `line_index` bigint unsigned NOT NULL COMMENT '行下标',
+  `create_time` timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '更新时间',
+  `is_delete` tinyint(0) NOT NULL DEFAULT 0 COMMENT '是否删除',
+  PRIMARY KEY (`uuid`) USING BTREE,
+  INDEX `Index_file_id`(`file_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for t_user
+-- ----------------------------
+DROP TABLE IF EXISTS `t_user`;
+CREATE TABLE `t_user`  (
+  `id` bigint(0) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '用户名',
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT '密码',
+  `create_date` timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_date` timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '更新时间',
+  `is_delete` tinyint(0) NOT NULL DEFAULT 0 COMMENT '是否已被删除',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_user
+-- ----------------------------
+INSERT INTO `t_user` VALUES (2, 'admin', '123', '2024-03-17 15:42:17', '2024-03-17 15:45:37', 0);
+
+SET FOREIGN_KEY_CHECKS = 1;
